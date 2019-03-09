86cd1d7039126d1743258ab1a7f16ed68774c2f7
sylviawan <sylvia.wan97@gmail.com>
2019-03-09 18:48:10 +0000

    Adding Card Swipe Method

--- FuudFinder/app/src/main/java/com/example/sylviawan/fuudfinder/Activities/SwipeActivity.java
+++ FuudFinder/app/src/main/java/com/example/sylviawan/fuudfinder/Activities/SwipeActivity.java
@@ -11,5 +11,10 @@ public class SwipeActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_swipe);
+
+        relativeLayoutContainer.setOnTouchListener(new onTouchListenser(){
+
+        });
+
     }
 }
