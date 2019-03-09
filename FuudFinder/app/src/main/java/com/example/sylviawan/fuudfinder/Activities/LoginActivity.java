71146972300e0b0987291f7cddb1fe6186da1233
sylviawan <sylvia.wan97@gmail.com>
2019-03-09 14:59:39 +0000

    Navigation Menu

--- FuudFinder/app/src/main/java/com/example/sylviawan/fuudfinder/Activities/LoginActivity.java
+++ FuudFinder/app/src/main/java/com/example/sylviawan/fuudfinder/Activities/LoginActivity.java
@@ -15,6 +15,7 @@ import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -98,4 +99,14 @@ public class LoginActivity extends AppCompatActivity {
     }
 
 
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+        FirebaseUser user = mAuth.getCurrentUser();
+
+        if (user != null){
+            updateUI();
+        }
+    }
 }
